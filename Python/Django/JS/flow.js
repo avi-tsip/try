3876b4687efc6d6ce53bf1936eb48844f38ee080
greenOnliner <atbiz@live.com>
2020-09-06 16:55:24 +0300

    added three new files

---
+++ Python/Django/JS/flow.js
@@ -0,0 +1,26 @@
+// if statements
+
+if (condition) {
+    // is true - execute this code;
+}
+else if(condition){
+    // is true - execute this code;
+}
+else {
+    //if prevoius conditions, didn't match, run this code;
+}
+
+// while loop
+
+while (condition) {
+    //execute while condition is true
+    //you can break from the loop by using the break key word
+    //
+}
+
+//for loops
+
+for (var i = 0; i < 5; i++) {
+    //execute code
+}
+
---
+++ Python/Django/JS/script.js
@@ -0,0 +1,9 @@
+alert('Welcome to the KG to lbs converter');
+
+var pounds = prompt('Please enter the weight in pounds :');
+
+var kilos = pounds * 0.45359237;
+
+alert('The weight in kilos is: ' + kilos);
+
+console.log('conversion completed');
