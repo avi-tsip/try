3b6c28cf4ef1c8ed93993c1d30e47f88f7f54137
AviTsip <avit@rapyd.net>
2023-06-18 10:38:33 +0300

    Added a lot of files

---
+++ C#/Learning/CSharpBasics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    public class LessonOne
+    {
+        public static void Main(string[] args)
+        {
+            float result = 0;
+            float numberOne = 0;
+            float numberTwo = 0;
+
+            Console.WriteLine("Enter your first number: ");
+            string firstNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter your second number: ");
+            string secondNumber = Console.ReadLine();
+
+            try
+            {
+                numberOne = float.Parse(firstNumber);
+                numberTwo = float.Parse(secondNumber);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The number is too long!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a number!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("The entry was empty, please add a valid number");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General error occured");
+            }
+
+            try
+            {
+                result = numberOne / numberTwo;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Dividign by zero is not allowed");
+            }
+            finally
+            {
+                Console.WriteLine("The result is: " + result);
+            }
+        }
+    }
+}
---
+++ Go/Main.go
@@ -0,0 +1,11 @@
+// You can edit this code!
+// Click here and start typing.
+package Main
+
+import "fmt"
+
+func main() {
+	var i int 
+	i = 42
+	fmt.Println(i)
+}
